d052866d23b43cb568f9a6ddbec3e8af9fdcb39f
tigrohvost <tigrohvost@gmail.com>
2023-05-23 01:40:34 -0400

    add random

--- components/back/back.py
+++ components/back/back.py
@@ -1,11 +1,15 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
 	return "It's alive! =^_____^="
+
+@app.route('/random', methods=['GET'])
+def get_random():
+      return str(random.randint(1,1000))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
